bc86165493a8ea4309505fbd2e46d3c8743a4e5d
Vaibhav Kesarwani <vaibhavkesarwani100@gmail.com>
2024-01-01 10:40:32 +0530

    js

--- Beginner/Basic/app.js
+++ Beginner/Basic/app.js
@@ -1,4 +1,4 @@
-console.table({name: "vaibhav", age: 20});
+console.table({ name: "vaibhav", age: 20 });
 
 // This is a simple comment
 
@@ -6,3 +6,36 @@ console.log();
 
 // Variables
 
+// (reserved word) (variable name) (type of value)
+
+// let, var and const
+
+// Declared variable
+
+let name = "10";
+
+// assign value to your variable
+
+let xyz;
+xyz = 10;
+
+console.log(name);
+console.log(xyz);
+
+// const variable can be assigned to multiple times.
+const youtubeChannel = "variable";
+//* shows you the error message
+// youtubeChannel = "vaibhav"
+
+console.log(youtubeChannel);
+
+// Challenge
+
+let userName = "vaibhav";
+let whatDoYouWannaBecomeInYourLife = "Programmer";
+let gender = "male";
+let twitterHandle = "Vaibhav-kesarwani";
+console.log(userName);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
