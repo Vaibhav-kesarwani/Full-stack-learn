3fba782f801b0734e5de2329ecdb88fb282b84b5
Vaibhav Kesarwani <vaibhavkesarwani100@gmail.com>
2024-01-03 15:14:38 +0530

    js

--- Beginner/Basic/app.js
+++ Beginner/Basic/app.js
@@ -105,3 +105,124 @@
 
 // Other that all the values are truthy values.
 
+// let notDefined = undefined;
+// console.log(notDefined);
+
+// notDefined = null;
+// console.log(notDefined);
+
+// let notDefined; // undefined
+
+//! challenge
+
+// let isJsProgrammingLanguage = true;
+// let isJsHard = false;
+// let favNumb = 10;
+
+// console.log(favNumb + undefined); // NaN
+
+// Comparison operators
+
+// -> Relational operators
+
+// > Greater than
+
+// console.log(10 > 10); //false
+
+// < Less than
+
+// console.log(10 < 10); // false
+
+// >= Greater than or equal to
+
+// console.log(10 >= 10); // true
+
+// <= Less than or equal to
+
+// console.log(10 <= 10); // true
+
+// -> Equality operators
+
+// === Strict equality (type + value)
+// !== Strict non-quality operators (type + value)
+// == lose equality operator (value)
+// != lose not equality operator (value)
+
+// console.log(10 === 10);
+// console.log(10 === '10');
+// console.log(10 !== '10');
+// console.log(10 == '10');
+// console.log(10 != '10');
+
+//! challenge
+
+// let firstFavNum = 10;
+// let secondFavNum = 8;
+
+// console.log(firstFavNum > secondFavNum);
+// console.log(firstFavNum < secondFavNum);
+// console.log(firstFavNum >= secondFavNum);
+// console.log(firstFavNum <= secondFavNum);
+// console.log(firstFavNum === secondFavNum);
+// console.log(firstFavNum == secondFavNum);
+// console.log(firstFavNum !== secondFavNum);
+// console.log(firstFavNum != secondFavNum);
+
+// Strings
+
+// let firstName = "vaibhav ";
+// let secondName = "kesarwani";
+
+// let fullName = firstName + " " + secondName;
+
+// console.log(firstName);
+// console.log(secondName);
+
+// console.log(fullName);
+
+// 1. Concatenation
+// let fullName = firstName.concat(secondName);
+// console.log(fullName);
+
+// 2. Append
+// firstName += "Something else";
+// console.log(firstName);
+
+// 3. Length
+// console.log(firstName.length);
+
+// 4. Cases
+// console.log(firstName.toUpperCase());
+// console.log(firstName.toLowerCase());
+
+// 5. Slice
+// console.log(firstName.slice(0, 3));
+
+// 6. Split & join
+// console.log(firstName.split(" ").join(" "));
+
+// 7. Includes
+// console.log(firstName.includes('i'));
+
+// 8. Trim
+// firstName = "                 vaibhav           ";
+// console.log(firstName.trim());
+
+// For writing the description
+
+// let description = `Heloo 
+// hi 
+// how 
+// are you
+// `;
+
+// console.log(description);
+ 
+// let firstName = "vaibhav ";
+// let secondName = "kesarwani";
+ 
+// let fullName = `${firstName} ${secondName}`;
+// console.log(fullName);
+
+//! challenge
+
