f5918eb196811aff921632afae2f68e4eda4b308
Vaibhav Kesarwani <vaibhavkesarwani100@gmail.com>
2024-01-13 20:00:17 +0530

    week 1.1

---
+++ week 1/week 1.1/app.js
@@ -0,0 +1,113 @@
+"use strict";
+
+// console.log("Hello world!");
+// const a = 1;
+// a = 2;
+
+// console.log(a);
+
+// datatype
+
+// string, boolean, number
+
+// let firstName = "vaibhav";
+// let age = 20;
+// let graduated = false;
+
+// console.log(firstName);
+// console.log(age);
+// console.log(graduated);
+
+// let sum = 0;
+
+// for (let i = 1; i <= 10; i++) {
+//     sum += i;
+// }
+
+// console.log(sum);
+
+// arrays
+
+// let persons = ["vaibhav", "yash", "vansh"];
+// let ages = [10, 20, 30];
+
+// console.log(persons);
+// console.log(ages);
+
+// write a program to print the even numbers
+
+// let num = [10, 20, 30, 40, 50, 63];
+// let numberLength = num.length;
+
+// for (let i = 0; i < numberLength; i++) {
+//     if (num[i] % 2 == 0) {
+//         console.log("even");
+//     }
+//     else {
+//         console.log("odd")
+//     }
+// }
+
+// objects
+
+// const user = {
+//   firstName: "vaibhav",
+//   age: "20",
+// };
+
+// console.log(user);
+
+// const validUser = {
+//   firstName: "",
+//   age: Number,
+//   graduate: Boolean,
+// };
+
+// console.log(validUser);
+
+// validUser.firstName = "vaibhav";
+// validUser.age = 20;
+// validUser.graduate = true;
+
+// console.log(validUser);
+
+// functions
+
+// function findSum(a, b) {
+//   return a + b;
+// }
+
+// console.log(findSum(2, 3));
+
+// let sum = 0;
+
+// for (let i = 0; i < 100000000000000; i++) {
+//     sum += i;
+// }
+
+// console.log(sum);
+
+// set timeout
+
+// function greet() {
+//   console.log("Hello world");
+// }
+
+// function hello() {
+//   console.log("Hello");
+// }
+
+// hello();
+
+// setTimeout(greet, 5 * 1000);
+
+// hello();
+
+// setInterval
+
+// function greet() {
+//     console.log("Hello world");
+// }
+
+// setInterval(greet, 1 * 1000);
+
