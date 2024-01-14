48dc9df9be00da130fde2572945daf32847df333
Vaibhav Kesarwani <vaibhavkesarwani100@gmail.com>
2024-01-14 10:26:56 +0530

    week 1.2

---
+++ week 1/week 1.2/app.js
@@ -0,0 +1,70 @@
+"use strict";
+
+// string
+
+// console.log(firstName.slice(0, 3));
+
+// const firstName = "vaibhav";
+
+// console.log(firstName.replace("v", "k"))
+
+// console.log(parseInt(3.90));
+
+// const a = [1, 2, 3];
+// const b = [4, 5, 6];
+// a.push(2);
+// a.shift();
+// a.unshift(2);
+// console.log(a);
+
+// console.log(a.concat(b));
+
+// for each
+
+// const a = [1, 2, 3, 4, 5, 6];
+
+// a.forEach((str) => console.log(str));
+
+// classes
+
+// class animal {
+//   constructor(name, legCount, speaks) {
+// this.name = name;
+// this.legCount = legCount;
+// this.speaks = speaks;
+//   }
+
+//   print()  {
+// console.log(0);
+//   }
+// }
+
+// let dog = new animal("dog", 4, "bhow bhow");
+// console.log(dog.print);
+// dog.print();
+
+// date
+
+// const currentDate = new Date();
+
+// console.log(currentDate.getDate());
+// console.log(currentDate.getTime());
+
+
+// function stopWatch() {
+//     let date = new Date();
+//     console.log(
+//     date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
+//   );
+// }
+
+// setInterval(stopWatch, 1 * 1000);
+
+// const user = {
+    // name: "vaibhav",
+    // gender: "male",
+    // age: 20,
+// };
+
+// console.log(JSON.stringify(user));
+
