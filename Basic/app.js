d01f830e8463f30ad41e0e9df9ababe7b15f7f80
Vaibhav Kesarwani <vaibhavkesarwani100@gmail.com>
2024-01-16 20:30:01 +0530

    added some basic folders

---
+++ Basic/app.js
@@ -0,0 +1,17 @@
+// var x = 10;
+
+// function example() {
+//   console.log(x);
+// }
+
+// example();
+// function example() {
+// console.log("Hello world!");
+// }
+
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// console.log(sum(2, 3));
+
