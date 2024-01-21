f1eb342385a5ab914a9010f71cc0b051a1fa1b3a
Vaibhav Kesarwani <vaibhavkesarwani100@gmail.com>
2024-01-21 19:30:43 +0530

    week 2.5

---
+++ Week 2/week 2.5/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+function calculation(n) {
+  let ans = 0;
+  for (let i = 1; i <= n; i++) {
+    ans += i;
+  }
+
+  return ans;
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+const app = express();
+
+app.get("/", (req, res) => {
+  const n = req.query.n;
+  // const a = parseInt(req.query.a);
+  // const b = parseInt(req.query.b);
+  // const ans = sum(a, b);
+  const ans = calculation(n);
+  res.send(ans.toString());
+  // res.send("Hello there!");
+});
+
+app.get("/home", (req, res) => {
+  res.send("Hello there!");
+});
+
+app.listen(3000);
